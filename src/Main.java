2968810c6dd816e0a9ca9eb56aa8776acb989b1d
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-01-31 08:51:15 +0100

    commit 1 sin fork

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Pull-request-con-fork
+ * @author Oliver Miguez Alonso
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
